abdfaeb0e2ad59ceb7f631d6b75c407d514e27ba
Marion Office <marionoffice@Marions-MacBook-Pro.local>
2024-10-14 21:44:15 -0500

    created some variables for class, possibleEventOrganizers is for list of event organizers, will be chosen after a certain person gets enough votes

---
+++ Event.h
@@ -0,0 +1,23 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <ctime>
+
+class Event {
+protected:
+    std::string eventName;
+    int eventDay;
+    std::vector<std::string> eventFunders;
+    std::string eventOrganizer;
+    std::vector<std::string> possibleEventOrganizers;
+    
+public:
+    
+    
+};
+
+#endif
